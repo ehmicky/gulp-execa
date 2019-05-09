34ad1b523c3b0d16c223d3b94b689608cda98994
ehmicky <ehmicky@gmail.com>
2019-05-09 10:00:00 +0200

    Fix task.displayName

--- src/task.js
+++ src/task.js
@@ -1,6 +1,5 @@
 import { parseOpts } from './options.js'
 import { execCommand } from './command.js'
-import { splitInput } from './split.js'
 
 // Create a Gulp task
 export const task = function(input, opts) {
@@ -8,19 +7,14 @@ export const task = function(input, opts) {
 
   const gulpTask = execCommand.bind(null, input, optsA)
 
-  setDisplayName({ gulpTask, input })
+  // We want to allow users to do `const gulpTask = execa(...)` instead of the
+  // more verbose `const gulpTask = () => execa(...)`. This is especially
+  // important when using `gulp.series()` or `gulp.parallel()`.
+  // However after binding a function or using a closure, assigning it to
+  // a variable does not change its `function.name` anymore. But this is
+  // used by Gulp as the displayed task name. So we use the command instead.
+  // eslint-disable-next-line fp/no-mutation
+  gulpTask.displayName = input
 
   return gulpTask
-}
-
-// We want to allow users to do `const gulpTask = execa(...)` instead of the
-// more verbose `const gulpTask = () => execa(...)`. This is especially
-// important when using `gulp.series()` or `gulp.parallel()`.
-// However after binding a function or using a closure, assigning it to
-// a variable does not change its `function.name` anymore. But this is
-// used by Gulp as the displayed task name. So we use the command instead.
-const setDisplayName = function({ gulpTask, input }) {
-  const { command } = splitInput({ input })
-  // eslint-disable-next-line fp/no-mutation, no-param-reassign
-  gulpTask.displayName = String(command)
 }
