d3ba966189084d6a3c86b78e1b17b6b0f4e0159d
ehmicky <ehmicky@gmail.com>
2019-05-09 10:00:00 +0200

    Remove error.message construction

--- src/command.js
+++ src/command.js
@@ -1,7 +1,7 @@
 import execa from 'execa'
+import PluginError from 'plugin-error'
 
 import { printEcho } from './echo.js'
-import { getError } from './error.js'
 
 // Fire the command with `execa()`
 export const execCommand = async function(input, opts) {
@@ -10,6 +10,21 @@ export const execCommand = async function(input, opts) {
   try {
     return await execa(input, opts)
   } catch (error) {
-    throw getError({ error, input, opts })
+    throw getError({ error })
   }
+}
+
+// Buld a Gulp error
+// TODO: when error is due to wrong input (not normal Execa error)
+// TODO: sometimes execa adds stdout|stderr, but we don't want that
+// (maybe removes anything after first newline?)
+export const getError = function({ error, error: { message } }) {
+  // TODO: try passing `error` instead of `error.message`
+  const errorA = new PluginError('gulp-execa', message, {
+    showProperties: false,
+  })
+  // Keep `execa` error properties
+  // eslint-disable-next-line fp/no-mutating-assign
+  Object.assign(errorA, error)
+  return errorA
 }
