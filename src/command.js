a79f4369f058335e982eb11e0b074e1781580e1e
ehmicky <ehmicky@gmail.com>
2019-05-09 10:00:00 +0200

    Remove command splitting

--- src/command.js
+++ src/command.js
@@ -1,17 +1,14 @@
 import execa from 'execa'
 
 import { printEcho } from './echo.js'
-import { splitInput } from './split.js'
 import { getError } from './error.js'
 
 // Fire the command with `execa()`
 export const execCommand = async function(input, opts) {
   printEcho({ input, opts })
 
-  const { command, args } = splitInput({ input })
-
   try {
-    return await execa(command, args, opts)
+    return await execa(input, opts)
   } catch (error) {
     const errorA = getError({ error, input, opts })
     throw errorA
