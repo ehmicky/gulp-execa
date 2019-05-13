9a58e756fd045bdd0ee563ca3a14d1ab7cb37885
ehmicky <ehmicky@gmail.com>
2019-05-13 10:00:00 +0200

    Start adding support for opts.result

--- src/stream.js
+++ src/stream.js
@@ -13,11 +13,14 @@ import { execCommand } from './exec.js'
 // call to those functions would be more efficient that creating lots of
 // child processes through streaming.
 export const stream = function(mapFunc, opts) {
-  const { maxConcurrency, ...optsA } = parseOpts({ opts, defaultOpts })
+  const { maxConcurrency, result: resultOpt, ...optsA } = parseOpts({
+    opts,
+    defaultOpts,
+  })
 
   return through.obj(
     { maxConcurrency },
-    execVinyl.bind(null, { mapFunc, opts: optsA }),
+    execVinyl.bind(null, { mapFunc, opts: optsA, resultOpt }),
   )
 }
 
@@ -29,14 +32,16 @@ const defaultOpts = {
   // We use `through2-concurrent` because `through2` processes files serially
   // The default is 16 which is too low
   maxConcurrency: 100,
+  // What to do with the result, among 'save', 'overwrite', 'stream' or 'create'
+  result: 'save',
 }
 
-const cExecVinyl = async function({ mapFunc, opts }, file) {
+const cExecVinyl = async function({ mapFunc, opts, resultOpt }, file) {
   const input = await mapFunc(file)
 
   const result = await fireCommand({ input, opts })
 
-  addToVinyl({ file, result })
+  handleResult[resultOpt]({ file, result })
 
   return file
 }
@@ -53,7 +58,11 @@ const fireCommand = function({ input, opts }) {
   return execCommand(input, opts)
 }
 
-const addToVinyl = function({ file, file: { execa = [] }, result }) {
+const saveResult = function({ file, file: { execa = [] }, result }) {
   // eslint-disable-next-line no-param-reassign, fp/no-mutation
   file.execa = [...execa, result]
+}
+
+const handleResult = {
+  save: saveResult,
 }
