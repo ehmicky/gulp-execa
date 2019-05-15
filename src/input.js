6964ce3c2a95be520f03f6ea7f7b4a8aa3349747
ehmicky <ehmicky@gmail.com>
2019-05-15 10:00:00 +0200

    Improve error message

--- src/input.js
+++ src/input.js
@@ -1,7 +1,7 @@
 // Validate main command and arguments
 export const validateInput = function({ input }) {
   if (!isValidInput({ input })) {
-    throw new Error('The command must be a non-empty string')
+    throw new Error(`The command must be a non-empty string: ${input}`)
   }
 }
 
