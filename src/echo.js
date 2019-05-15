24fdb3e81abb32a8c8c761afe20ca83cbbd7c3f1
ehmicky <ehmicky@gmail.com>
2019-05-15 10:00:00 +0200

    Add comma in comment

--- src/echo.js
+++ src/echo.js
@@ -1,7 +1,7 @@
 import fancyLog from 'fancy-log'
 import { cyan } from 'chalk'
 
-// If `opts.echo` is `true` echo the command on the terminal
+// If `opts.echo` is `true`, echo the command on the terminal
 export const printEcho = function({ input, opts: { echo } }) {
   if (!echo) {
     return
