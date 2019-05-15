b1a15eea53fa633e2bbe40c77e5c930c70860f8e
ehmicky <ehmicky@gmail.com>
2019-05-15 10:00:00 +0200

    Fix comment

--- src/exec.js
+++ src/exec.js
@@ -5,14 +5,7 @@ import { validateInput } from './input.js'
 import { parseOpts } from './options.js'
 import { printEcho } from './echo.js'
 
-// Execute a shell command
-// To create a Gulp task, one should not use `bind()` as it removes
-// `Function.name`. Instead one should do `const taskName = () => exec(...)`
-// We avoid `exec.shell()` as it leads to shell-specific input which is not
-// cross-platform.
-// We avoid using a single string as input and tokenizing it as it's difficult
-// with whitespaces escaping. Also escaping is shell-specific, e.g. on Windows
-// `cmd.exe` only use double quotes not single quotes.
+// Execute a child process (command + arguments)
 export const exec = function(input, opts) {
   validateInput({ input })
   const optsA = parseOpts({ opts })
