e2a4d1a601f5f414bb49e178ad9200b71d0cd36d
ehmicky <ehmicky@gmail.com>
2019-05-17 10:00:00 +0200

    Add comments

--- test/helpers/methods.js
+++ test/helpers/methods.js
@@ -1,11 +1,17 @@
+// We repeat most tests for each API method
 export const STREAM_METHODS = [
+  // `gulp.src(...).pipe(stream(..., { result: 'replace' }))`
   { suffix: 'stream-buffer', method: 'stream' },
+  // `gulp.src(..., { buffer: false }).pipe(stream(..., { result: 'replace' }))`
   { suffix: 'stream-stream', method: 'stream', buffer: false },
+  // `gulp.src(...).pipe(stream(..., { result: 'save' }))`
   { suffix: 'stream-save', method: 'stream', opts: { result: 'save' } },
 ]
 
 export const METHODS = [
+  // `exec(...)`
   { suffix: 'exec', method: 'exec' },
+  // `task(...)`
   { suffix: 'task', method: 'task' },
   ...STREAM_METHODS,
 ]
