7160605911663bee271a07cd91ef6af288eab9f8
ehmicky <ehmicky@gmail.com>
2019-05-17 10:00:00 +0200

    Fix tests

--- test/input.js
+++ test/input.js
@@ -14,7 +14,7 @@ testEach(
 
 testEach(
   METHODS,
-  [{ opts: { encoding: 'invalid' } }, { command: 'invalid', read: false }],
+  [{ opts: { uid: 0.5 } }, { command: 'invalid', read: false }],
   (suffix, methodProps, data) =>
     test(`Errored command ${suffix}`, t =>
       snapshotTest({ t, methodProps, data })),
