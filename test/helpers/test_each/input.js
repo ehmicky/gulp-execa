a6aa98c0b9eba178b65a7085a04ccdbf873023b0
ehmicky <ehmicky@gmail.com>
2019-05-18 10:00:00 +0200

    Better validation in test-each

--- test/helpers/test_each/input.js
+++ test/helpers/test_each/input.js
@@ -1,11 +1,22 @@
 // Parse and validate main input
 export const parseInput = function(inputArgs) {
   const iterables = inputArgs.slice(0, -1)
+  iterables.forEach(validateIterable)
 
   const func = inputArgs[inputArgs.length - 1]
   validateFunc(func)
 
   return { iterables, func }
+}
+
+const validateIterable = function(iterable) {
+  if (iterable[Symbol.iterator] === undefined && !isRepeat(iterable)) {
+    throw new TypeError(`Argument must be an iterable or a positive integer: ${iterable}`)
+  }
+}
+
+export const isRepeat = function(iterable) {
+  return Number.isInteger(iterable) && iterable >= 0
 }
 
 const validateFunc = function(func) {
