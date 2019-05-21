558eb7168bab0f4b8082743dea7035ea7dd6b38f
ehmicky <ehmicky@gmail.com>
2019-05-21 10:00:00 +0200

    Improve test-each duplicates

--- test/helpers/test_each/duplicate.js
+++ test/helpers/test_each/duplicate.js
@@ -1,16 +1,28 @@
 // Ensure names are unique by appending the index when we find duplicates
 export const fixDuplicate = function(loop, index, loops) {
-  if (!isDuplicate(loop, loops)) {
+  const duplicateIndex = getDuplicateIndex(loop, loops)
+
+  if (duplicateIndex === undefined) {
     return loop
   }
 
   const { name } = loop
-  const nameA = `${name} (${index})`
+  const nameA = `${name} (${duplicateIndex})`
   return { ...loop, name: nameA }
 }
 
-const isDuplicate = function(loop, loops) {
-  return loops.some(
-    loopA => loopA.name === loop.name && loopA.index !== loop.index,
-  )
+// The duplicate index is scoped to each specific duplicates group.
+// This makes the duplicate indexes more stable:
+//   - when changing other non-duplicate data
+//   - when changing other duplicate data in the same iterable
+//   - this is important in case the `name` is used in test snapshots
+// This also makes more sense for the users.
+const getDuplicateIndex = function({ name, index }, loops) {
+  const duplicateLoops = loops.filter(loop => loop.name === name)
+
+  if (duplicateLoops.length === 1) {
+    return
+  }
+
+  return duplicateLoops.findIndex(loop => loop.index === index)
 }
