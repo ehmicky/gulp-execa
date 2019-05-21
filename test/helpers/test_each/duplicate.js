4d8fe14a06d85a2b92d3f203042597fdad4bde28
ehmicky <ehmicky@gmail.com>
2019-05-21 10:00:00 +0200

    Rename variable

--- test/helpers/test_each/duplicate.js
+++ test/helpers/test_each/duplicate.js
@@ -1,23 +1,23 @@
-// Ensure names are unique by appending the index when we find duplicates
+// Ensure names are unique by appending a counter when we find duplicates
 export const fixDuplicate = function(loop, index, loops) {
-  const duplicateIndex = getDuplicateIndex(loop, loops)
+  const duplicateCounter = getDuplicateCounter(loop, loops)
 
-  if (duplicateIndex === undefined) {
+  if (duplicateCounter === undefined) {
     return loop
   }
 
   const { name } = loop
-  const nameA = `${name} (${duplicateIndex})`
+  const nameA = `${name} (${duplicateCounter})`
   return { ...loop, name: nameA }
 }
 
-// The duplicate index is scoped to each specific duplicates group.
-// This makes the duplicate indexes more stable:
+// The duplicate counter is scoped to each specific duplicates group.
+// This makes the duplicate counters more stable:
 //   - when changing other non-duplicate data
 //   - when changing other duplicate data in the same iterable
 //   - this is important in case the `name` is used in test snapshots
 // This also makes more sense for the users.
-const getDuplicateIndex = function({ name, index }, loops) {
+const getDuplicateCounter = function({ name, index }, loops) {
   const duplicateLoops = loops.filter(loop => loop.name === name)
 
   if (duplicateLoops.length === 1) {
