abc91aedac6ed833b23a7e1830a0cc34d4e784cb
ehmicky <ehmicky@gmail.com>
2021-02-28 11:25:52 +0100

    Use colors-option

--- src/echo.js
+++ src/echo.js
@@ -1,5 +1,7 @@
-import { cyan } from 'chalk'
+import colorsOption from 'colors-option'
 import fancyLog from 'fancy-log'
+
+const { cyan } = colorsOption()
 
 // If `opts.echo` is `true`, echo the command and its arguments on the terminal
 export const printEcho = function ({ input, opts: { echo } }) {
